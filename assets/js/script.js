efab8c6b059361c25e5a9bd4099e5a6a05f72b4c
Rekiya <rekiygemini@gmail.com>
2026-09-12 12:52:24 +0200

    restructured script

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,69 +10,6 @@ function renderImgs() {
   contentRef.innerHTML = htmlContent;
 }
 
-function updateDialogImage() {
-  const dialogImg = document.getElementById("dialog-rendered");
-  dialogImg.src = imageArray[currentIndex].src;
-  dialogImg.alt = imageArray[currentIndex].alt;
-  dialogImg.width = imageArray[currentIndex].width;
-  dialogImg.height = imageArray[currentIndex].height;
-}
-
-function updateDialogInfo() {
-  document.getElementById("dialog-caption").textContent = imageArray[currentIndex].alt;
-  document.getElementById("dialog-counter").textContent = `${currentIndex + 1} / ${imageArray.length} `;
-  document.getElementById("dialog-price").innerText = "$" + imageArray[currentIndex].price;
-}
-
-function updateDialogLikes() {
-  const likeBtn = document.getElementById("like-button");
-  const likeCountRef = document.getElementById("like-count");
-
-  likeBtn.classList.toggle("is-liked", imageArray[currentIndex].liked === true);
-  likeCountRef.innerText = imageArray[currentIndex].likes;
-}
-
-function updateDialogContent() {
-  updateDialogImage();
-  updateDialogInfo();
-  updateDialogLikes();
-  renderComments();
-}
-
-function toggleLike() {
-  if (imageArray[currentIndex].liked === true) {
-    imageArray[currentIndex].liked = false;
-    imageArray[currentIndex].likes -= 1;
-  } else {
-    imageArray[currentIndex].liked = true;
-    imageArray[currentIndex].likes += 1;
-  }
-}
-
-function likeCounter() {
-  toggleLike();
-  updateDialogLikes();
-}
-
-function renderComments() {
-  let commentRef = document.getElementById("comments-list");
-  commentRef.innerHTML = "";
-
-  for (let i = 0; i < imageArray[currentIndex].comments.length; i++) {
-    commentRef.innerHTML += commentsTemplate(i);
-  }
-}
-
-function addComments() {
-  let inputContent = document.getElementById("comment-input");
-  let inputContentValue = inputContent.value;
-
-  imageArray[currentIndex].comments.push({ name: "Username", comment: inputContentValue });
-
-  inputContent.value = "";
-  renderComments();
-}
-
 function renderDialog(event) {
   const target = event.target.closest("button");
 
@@ -94,6 +31,57 @@ function changeDialog(step) {
   updateDialogContent();
 }
 
+function updateDialogImage() {
+  const dialogImg = document.getElementById("dialog-rendered");
+  dialogImg.src = imageArray[currentIndex].src;
+  dialogImg.alt = imageArray[currentIndex].alt;
+  dialogImg.width = imageArray[currentIndex].width;
+  dialogImg.height = imageArray[currentIndex].height;
+}
+
+function updateDialogInfo() {
+  document.getElementById("dialog-caption").textContent = imageArray[currentIndex].alt;
+  document.getElementById("dialog-counter").textContent = `${currentIndex + 1} / ${imageArray.length} `;
+  document.getElementById("dialog-price").innerText = "$" + imageArray[currentIndex].price;
+}
+
+function toggleLike() {
+  if (imageArray[currentIndex].liked === true) {
+    imageArray[currentIndex].liked = false;
+    imageArray[currentIndex].likes -= 1;
+  } else {
+    imageArray[currentIndex].liked = true;
+    imageArray[currentIndex].likes += 1;
+  }
+}
+
+function updateLikeDisplay() {
+  const likeBtn = document.getElementById("like-button");
+  const likeCountRef = document.getElementById("like-count");
+
+  likeBtn.classList.toggle("is-liked", imageArray[currentIndex].liked === true);
+  likeCountRef.innerText = imageArray[currentIndex].likes;
+}
+
+function addComments() {
+  let inputContent = document.getElementById("comment-input");
+  let inputContentValue = inputContent.value;
+
+  imageArray[currentIndex].comments.push({ name: "Username", comment: inputContentValue });
+
+  inputContent.value = "";
+  renderComments();
+}
+
+function renderComments() {
+  let commentRef = document.getElementById("comments-list");
+  commentRef.innerHTML = "";
+
+  for (let i = 0; i < imageArray[currentIndex].comments.length; i++) {
+    commentRef.innerHTML += commentsTemplate(i);
+  }
+}
+
 function onBackdropClick(event) {
   const closeDialogOutside = document.getElementById("image-dialog");
 
@@ -102,11 +90,22 @@ function onBackdropClick(event) {
   }
 }
 
+function updateDialogContent() {
+  updateDialogImage();
+  updateDialogInfo();
+  updateLikeDisplay();
+  renderComments();
+}
+
 function init() {
   renderImgs();
   document.getElementById("photo-grid").addEventListener("click", renderDialog);
   document.getElementById("dialog-back").addEventListener("click", () => changeDialog(-1));
   document.getElementById("dialog-next").addEventListener("click", () => changeDialog(1));
+  document.getElementById("like-button").addEventListener("click", () => {
+    toggleLike();
+    updateLikeDisplay();
+  });
   document.getElementById("image-dialog").addEventListener("click", onBackdropClick);
 }
 
